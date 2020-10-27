a4a2acb651946bcd5a3ba1f3cdb475e189e66643
asshidiq30 <a.asshidiq@gmail.com>
2020-10-28 03:18:17 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae pariatur dolor ea rem nisi minima porro eos deserunt quas, adipisci blanditiis perspiciatis eius modi vero ducimus voluptates explicabo minus illum!</p>
 </body>
 </html>
